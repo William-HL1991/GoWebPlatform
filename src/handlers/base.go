5f2f69aa4e13e861ed1caa22cdc41ef03f95ed7d
huanglei09 <huanglei09@baidu.com>
2019-03-26 11:21:20 +0800

    换电脑了先提上去

--- src/handlers/base.go
+++ src/handlers/base.go
@@ -6,9 +6,41 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
-func Index(w http.ResponseWriter, r *http.Request)  {
+//var appSession *sessions.Manager
+//
+//// 初始化session manager
+//func init() {
+//	appSession, _ = sessions.NewSessionManager("memory", "sessionid", 3600)
+//	go appSession.GC()
+//}
+
+
+// cookies
+
+func setCookie(w http.ResponseWriter, name string , vlaue string) {
+	now := time.Now()
+	d, _ := time.ParseDuration("1H")
+	newNow := now.Add(d)
+	fmt.Println(newNow)
+	cookie := http.Cookie{Name: name, Value: vlaue, Expires:newNow}
+	http.SetCookie(w, &cookie)
+	fmt.Println("cookie 过期时间", cookie.Expires)
+}
+
+func readCookie(w http.ResponseWriter, r *http.Request) {
+	for _, cookie := range r.Cookies() {
+		fmt.Println("cookie:", cookie.Name)
+		fmt.Println("cookie", cookie.Value)
+		fmt.Println("cookie", cookie.Expires)
+	}
+
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	readCookie(w, r)
 	resp := utils.NewBaseJsonBean()
 	resp.Code = 0
 	resp.Message = "链接正常"
@@ -77,6 +109,8 @@ func UserLogin(w http.ResponseWriter, r *http.Request) {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
+			// cookies
+			setCookie(w, username, "logined")
 			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 			w.WriteHeader(http.StatusOK)
 			resp := utils.NewBaseJsonBean()
